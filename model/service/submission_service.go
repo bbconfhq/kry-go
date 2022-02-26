ad1a6cd281ae8e037028af26d8407b8ef054f71e
Ryo <9062624+gwanryo@users.noreply.github.com>
2022-02-26 22:48:06 +0900

    Write blueprint of submission

--- model/service/submission_service.go
+++ model/service/submission_service.go
@@ -1,7 +1,74 @@
 package service
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"kry-go/model/entity"
+	"kry-go/model/request"
+	"kry-go/model/response"
+)
 
 type SubmissionService struct {
 	DB *gorm.DB
+}
+
+func (s *SubmissionService) SelectSubmissionsByFilter(offset int, id int, language string, limit int) ([]response.SubmissionResponse, error) {
+	var submissions []entity.Submission
+	var submissionsResponse []response.SubmissionResponse
+
+	clauses := make([]clause.Expression, 0)
+
+	if id != -1 {
+		clauses = append(clauses, clause.Eq{Column: "user_id", Value: id})
+	}
+
+	if language != "" {
+		clauses = append(clauses, clause.Eq{Column: "language", Value: language})
+	}
+
+	result := s.DB.
+		Clauses(clauses...).
+		Order("id").
+		Offset(offset).
+		Limit(limit).
+		Find(&submissions)
+
+	if result.Error != nil {
+		return submissionsResponse, result.Error
+	}
+
+	for _, e := range submissions {
+		submissionsResponse = append(submissionsResponse, response.SubmissionResponse{
+			Result:    e.Result,
+			Language:  e.Language,
+			Code:      e.Code,
+			UserID:    e.UserID,
+			ProblemID: e.ProblemID,
+		})
+	}
+
+	return submissionsResponse, nil
+}
+
+func (s *SubmissionService) CreateSubmission(r *request.SubmissionRequest) error {
+	submission := entity.Submission{
+		Result:    r.Result,
+		Language:  r.Language,
+		Code:      r.Code,
+		UserID:    r.UserID,
+		ProblemID: r.ProblemID,
+	}
+
+	result := s.DB.Omit("User").Omit("Problem").Create(&submission)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (s *SubmissionService) SelectSubmission(id int) (response.SubmissionResponse, error) {
+	var submission response.SubmissionResponse
+
+	return submission, nil
 }
--- routes/handler/submission_handler.go
+++ routes/handler/submission_handler.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"github.com/labstack/echo/v4"
+	"kry-go/model/request"
 	"kry-go/model/service"
 	"kry-go/server"
 	"net/http"
+	"strconv"
 )
 
 type SubmissionHandler struct {
@@ -23,14 +25,33 @@ func MakeSubmissionHandler(server *server.Server) *SubmissionHandler {
 // @Accept        json
 // @Produce       json
 // @Param         page		query	uint	true	"Page of submissions"	minimum(1)
-// @Param         user_id	query	string	false	"User ID"
+// @Param         user_id	query	uint	false	"User ID"
 // @Param         language	query	string	false	"Type of language"
 // @Success       200	{array}		response.SubmissionResponse
 // @Failure       400	{object}	echo.HTTPError
 // @Failure       500	{object}	echo.HTTPError
 // @Router        /submission [get]
 func (h *SubmissionHandler) GetSubmissions(c echo.Context) error {
-	return c.JSON(http.StatusOK, 0)
+	page, err := strconv.ParseInt(c.QueryParam("page"), 10, 0)
+
+	if err != nil || page <= 0 {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	id, err := strconv.ParseInt(c.QueryParam("user_id"), 10, 0)
+	if err != nil {
+		id = -1
+	}
+
+	language := c.QueryParam("language")
+
+	offset := int(100 * (page - 1))
+	result, err := h.service.SelectSubmissionsByFilter(offset, int(id), language, offset)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, result)
 }
 
 // PostSubmission godoc
@@ -45,7 +66,17 @@ func (h *SubmissionHandler) GetSubmissions(c echo.Context) error {
 // @Failure       500	{object}	echo.HTTPError
 // @Router        /submission [post]
 func (h *SubmissionHandler) PostSubmission(c echo.Context) error {
-	return c.JSON(http.StatusOK, 0)
+	var submissionRequest request.SubmissionRequest
+
+	if err := (&echo.DefaultBinder{}).BindBody(c, &submissionRequest); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if err := h.service.CreateSubmission(&submissionRequest); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, nil)
 }
 
 // GetSubmission godoc
@@ -54,11 +85,51 @@ func (h *SubmissionHandler) PostSubmission(c echo.Context) error {
 // @Tags         Submission Endpoints
 // @Accept       json
 // @Produce      json
-// @Param        submission_id	path	uint					true	"Submission ID"
+// @Param        id	path	uint					true	"Submission ID"
 // @Success      200	{object}	response.SubmissionResponse
 // @Failure      400	{object}	echo.HTTPError
 // @Failure      500	{object}	echo.HTTPError
-// @Router       /submission/{submission_id} [get]
+// @Router       /submission/{id} [get]
 func (h *SubmissionHandler) GetSubmission(c echo.Context) error {
-	return c.JSON(http.StatusOK, 0)
+	id, err := strconv.ParseInt(c.QueryParam("id"), 10, 0)
+	if err != nil || id <= 0 {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	result, err := h.service.SelectSubmission(int(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+// PutSubmission godoc
+// @Summary      TODO: Put submission detail
+// @Description  Edit submission detail
+// @Tags         Submission Endpoints
+// @Accept       json
+// @Produce      json
+// @Param        submission	body	request.SubmissionRequest	true	"Necessary submission information"
+// @Success      201	{object}	nil
+// @Failure      400	{object}	echo.HTTPError
+// @Failure      500	{object}	echo.HTTPError
+// @Router       /submission/{id} [put]
+func (h *SubmissionHandler) PutSubmission(c echo.Context) error {
+	return c.JSON(http.StatusOK, nil)
+}
+
+// DeleteSubmission godoc
+// @Summary      TODO: Delete submission detail
+// @Description  Delete submission detail
+// @Tags         Submission Endpoints
+// @Accept       json
+// @Produce      json
+// @Param        id	path	uint					true	"Contest ID"
+// @Success      204	{object}	nil
+// @Failure      400	{object}	echo.HTTPError
+// @Failure      500	{object}	echo.HTTPError
+// @Router       /submission/{id} [delete]
+func (h *SubmissionHandler) DeleteSubmission(c echo.Context) error {
+	return c.JSON(http.StatusOK, nil)
 }
