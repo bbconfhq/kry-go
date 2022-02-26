09c9c7fa37b3e8d6b560519ec322deefe2859304
Ryo <9062624+gwanryo@users.noreply.github.com>
2022-02-26 22:49:17 +0900

    Write spec of tag

--- routes/handler/tag_handler.go
+++ routes/handler/tag_handler.go
@@ -16,22 +16,77 @@ func MakeTagHandler(server *server.Server) *TagHandler {
 	return &TagHandler{server: server}
 }
 
+// GetTags godoc
+// @Summary       Get tags
+// @Description   Get tags
+// @Tags          Tag Endpoints
+// @Accept        json
+// @Produce       json
+// @Param         page		query	uint	true	"Page of tags"	minimum(1)
+// @Success       200	{array}		response.TagResponse
+// @Failure       400	{object}	echo.HTTPError
+// @Failure       500	{object}	echo.HTTPError
+// @Router        /tag [get]
 func (h *TagHandler) GetTags(c echo.Context) error {
 	return c.JSON(http.StatusOK, 0)
 }
 
+// PostTag godoc
+// @Summary       Post tag
+// @Description   Register tag
+// @Tags          Tag Endpoints
+// @Accept        json
+// @Produce       json
+// @Param         tag	body	request.TagRequest	true	"Necessary tag information"
+// @Success       201	{object}	nil
+// @Failure       400	{object}	echo.HTTPError
+// @Failure       500	{object}	echo.HTTPError
+// @Router        /tag [post]
 func (h *TagHandler) PostTag(c echo.Context) error {
 	return c.JSON(http.StatusOK, 0)
 }
 
+// GetTag godoc
+// @Summary      Get tag detail
+// @Description  Get tag detail
+// @Tags         Tag Endpoints
+// @Accept       json
+// @Produce      json
+// @Param        id	path	uint					true	"Tag ID"
+// @Success      200	{object}	response.TagResponse
+// @Failure      400	{object}	echo.HTTPError
+// @Failure      500	{object}	echo.HTTPError
+// @Router       /tag/{id} [get]
 func (h *TagHandler) GetTag(c echo.Context) error {
 	return c.JSON(http.StatusOK, 0)
 }
 
+// PutTag    godoc
+// @Summary      Put tag detail
+// @Description  Edit tag detail
+// @Tags         Tag Endpoints
+// @Accept       json
+// @Produce      json
+// @Param        problem	body	request.TagRequest	true	"Necessary tag information"
+// @Success      201	{object}	nil
+// @Failure      400	{object}	echo.HTTPError
+// @Failure      500	{object}	echo.HTTPError
+// @Router       /tag/{id} [put]
 func (h *TagHandler) PutTag(c echo.Context) error {
 	return c.JSON(http.StatusOK, 0)
 }
 
+// DeleteTag godoc
+// @Summary      Delete tag detail
+// @Description  Delete tag detail
+// @Tags         Tag Endpoints
+// @Accept       json
+// @Produce      json
+// @Param        id	path	uint					true	"Tag ID"
+// @Success      204	{object}	nil
+// @Failure      400	{object}	echo.HTTPError
+// @Failure      500	{object}	echo.HTTPError
+// @Router       /tag/{id} [delete]
 func (h *TagHandler) DeleteTag(c echo.Context) error {
 	return c.JSON(http.StatusOK, 0)
 }
